c86128e8db90179e5101066c68e0f4cb4e9fa901
MarMaps <mgolubeva468@gmail.com>
2024-11-10 17:08:57 +0500

    finish num1

--- ЛР1. Записи и массивы/1sourse.pas
+++ ЛР1. Записи и массивы/1sourse.pas
@@ -7,50 +7,37 @@ type
 		gender: string;
 		date_b: string; //позже расписать отдельно
 		id_person: string;
-		id_children: {array of }string; //string
+		id_children: string;
+		Achildren: array of string;
 end;
 
 var
 	data_out: array of Tdata_people;
 	person: Tdata_people;
-	i,j,k,h,n: integer;
+	i, j, k, g: integer;
 	s: string; //для пустой строки
 
 begin
-	i := 0;
-	n := 0;
+i := 0;
     while not eof do
     begin
         readln(person.fio);
         readln(person.gender);
         readln(person.date_b);
         readln(person.id_person);
-        readln(person.id_children);
-        
+        SetLength(person.Achildren, 0);
         readln(s);
+        
         while s <> '' do
         begin
-			//readln(person.id_children);
-			readln(s);
-			for h := 1 to n do begin
-				readln(person.id_children);
-				readln(s);
-				//n := n + 1;
-			end;
+			SetLength(person.Achildren, Length(person.Achildren) + 1); // сколько детей на столько и увеличение
+			person.Achildren[High(person.Achildren)] := s; // сохранение уд/л ребенка----
+			readln(s); 
 		end;
-		
-		{readln(s);
-        while s <> '' do
-        begin
-			person.id_children[n] := s;
-			n := n + 1;
-            readln(s);
-        end;
-        n := 0;}
     
 		SetLength(data_out, i + 1); 
-		data_out[i] := person; //сохранение человека в массив
-		i := i + 1;                
+		data_out[i] := person; //сохранение чела в массив
+		i := i + 1;             
 	end;
 
   k := 1;
@@ -59,10 +46,14 @@ begin
 		writeln(k, ') ', 'ФИО: ', data_out[j].fio);
 		writeln('пол: ', data_out[j].gender);
 		writeln('дата рождения: ', data_out[j].date_b);
-		writeln('номер удостоверения личности: ', data_out[j].id_person);
-		writeln('номер уд/л детей: ', data_out[j].id_children);
+		writeln('номер уд/л: ', data_out[j].id_person);
+		writeln('Номер уд/л детей:');
+		for g := 0 to Length(data_out[j].Achildren) - 1 do
+			writeln(data_out[j].Achildren[g]);
+			
 		writeln(i,'|', j);
 		k := k + 1;
 		writeln();
 	end;
 end.
+//расписать date_b
